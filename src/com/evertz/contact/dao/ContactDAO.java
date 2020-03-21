9da30aa246693db1f194487e8e8a6632cbea0a69
supreeths8 <supreeths8@gmail.com>
2020-03-21 20:37:52 +0530

    first commit

---
+++ src/com/evertz/contact/dao/ContactDAO.java
@@ -0,0 +1,18 @@
+package com.evertz.contact.dao;
+
+import java.util.List;
+
+import com.evertz.contact.model.Contact;
+
+public interface ContactDAO {
+	public int save(Contact contact);
+
+	public int update(Contact contact);
+
+	public Contact get(Integer id);
+
+	public int delete(Integer id);
+
+	public List<Contact> list();
+	
+}
