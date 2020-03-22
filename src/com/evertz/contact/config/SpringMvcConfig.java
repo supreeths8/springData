4100bd0e7589fc173476d90e5a098e238075e7aa
supreeths8 <supreeths8@gmail.com>
2020-03-22 16:17:29 +0530

    dao and config done. controller remaining

---
+++ src/com/evertz/contact/config/SpringMvcConfig.java
@@ -0,0 +1,46 @@
+package com.evertz.contact.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.evertz.contact.dao.ContactDAO;
+import com.evertz.contact.dao.ContactDAOImpl;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.evertz.contact")
+public class SpringMvcConfig implements WebMvcConfigurer{
+	
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+		
+	}
+	
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Bean
+	public ContactDAO getContactDAO(){
+		return new ContactDAOImpl(getDataSource());
+	}
+}
