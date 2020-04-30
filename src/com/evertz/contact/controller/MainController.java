b42158dab26284ea2c41a98fbdba2cb6d6de8d35
supreeths8 <supreeths8@gmail.com>
2020-05-01 02:44:52 +0530

    session setting

--- src/com/evertz/contact/controller/MainController.java
+++ src/com/evertz/contact/controller/MainController.java
@@ -1,17 +1,13 @@
 package com.evertz.contact.controller;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.evertz.contact.model.Admin;
